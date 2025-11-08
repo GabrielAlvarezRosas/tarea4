1bb6a3a76c4bcb6a75d0e7f05eadb31364f7ada6
Gabriel Alvarez Rosas <franklinalvarez1307005@gmail.com>
2025-11-08 12:27:25 -0500

    txt

--- Tarea_4/obj/Debug/net6.0/Tarea_4.AssemblyInfo.cs
+++ Tarea_4/obj/Debug/net6.0/Tarea_4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tarea_4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2eae0d1d2953ba493bc2238c24e617031cc169f8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc3228ec6c03d0972cb281cae94cd3cca9298b04")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tarea_4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tarea_4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
